ad3ec3ac92d66b60b4e2c38a623810e57fd9baba
Chhun Meyling <lingm8668@gmail.com>
2025-06-12 10:44:04 +0700

    last update

--- src/main/java/com/example/spring_ioc_demo/SpringIocDemoApplication.java
+++ src/main/java/com/example/spring_ioc_demo/SpringIocDemoApplication.java
@@ -86,30 +86,7 @@ public class SpringIocDemoApplication  {
 
 				}
 			}
-//			System.out.println("=== Initial Students ===");
-//			studentService.getAllStudents().forEach(System.out::println);
-//
-//			System.out.println("\n=== Get Student By ID (3) ===");
-//			System.out.println("\n			============== The Output is ==================");
-//			System.out.println(studentService.getStudentById(3));
-//
-//			System.out.println("\n=== Add New Student ===");
-//			System.out.println("\n			============== The Output is ==================");
-//			Student newStudent = new Student(11, "Leo Carter", "Male", 80.5);
-//
-//
-//			System.out.println("\n=== Update Student ID (2) ===");
-//			Student updated = new Student(2, "Bob Smith Jr.", "Male", 85.0);
-//			boolean updatedStatus = studentService.updateStudentById(2, updated);
-//			System.out.println("Update Successful: " + updatedStatus);
-//			System.out.println("\n			============== The Output is ==================");
-//			studentService.getAllStudents().forEach(System.out::println);
-//
-//			System.out.println("\n=== Delete Student ID (1) ===");
-//			boolean deletedStatus = studentService.deleteStudentById(1);
-//			System.out.println("Delete Successful: " + deletedStatus);
-//			System.out.println("\n			============== The Output is ==================");
-//			studentService.getAllStudents().forEach(System.out::println);
+
 		};
 	}
 
